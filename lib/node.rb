c2d170ba80c973943ead5444fa94a7101c2d99fa
Dean <72749793+drossano@users.noreply.github.com>
2022-02-12 13:45:30 -0500

    Add comparable method to compare Nodes using data attribute

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,9 @@
 class Node
+  include Comparable
   attr_accessor :data, :left_child, :right_child
-  
+  def <=>(other_node)
+    data <=> other_node.data
+  end
   def initialize(data = nil, left_child = nil, right_child = nil)
     @data = data
     @left_child = left_child
