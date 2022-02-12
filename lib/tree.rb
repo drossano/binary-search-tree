75d26591c3c76778a278bca711cde344259bce1f
Dean <72749793+drossano@users.noreply.github.com>
2022-02-12 14:56:29 -0500

    Build #build_tree and copy in #pretty_print

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,26 @@
+require_relative 'node'
+require 'pry-byebug'
 class Tree
   def initialize(array)
     @data = array.uniq.sort
+    p @data
+    @data_end = @data.length - 1
+    bst = build_tree(@data, @data_end)
+  end
+
+  def build_tree(array, array_end, array_start = 0)
+    #binding.pry
+    return nil if array_start > array_end
+    mid = (array_start + array_end) / 2
+    root = Node.new(array[mid])
+    root.left_child = build_tree(array, mid - 1, array_start)
+    root.right_child = build_tree(array, array_end, mid + 1)
+    @root = root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
 end
