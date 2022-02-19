e54a9b90f07b2fd972587d1f9c948b32fcc084da
Dean <72749793+drossano@users.noreply.github.com>
2022-02-18 21:51:53 -0500

    Add link to replit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Binary Search Tree
 This project was created as part of [The Odin Project](https://www.theodinproject.com/) Ruby curriculum. The goal of this project is to create a balanced binary search tree and various methods to manipulate and traverse the tree.
+
+You can either run the driver script [here](https://replit.com/@drossano/Binary-Search-Tree#main.rb) or download the files and run the main.rb file.
--- main.rb
+++ main.rb
@@ -3,30 +3,30 @@ require_relative './lib/tree'
 tree = Tree.new(Array.new(15) { rand(1..100) })
 tree.pretty_print
 puts "Is the tree balanced: #{tree.balanced?}"
-puts "Elements in level order:"
+puts 'Elements in level order:'
 p tree.level_order
-puts "Elements in preorder:"
+puts 'Elements in preorder:'
 p tree.preorder
-puts "Elements in postorder:"
+puts 'Elements in postorder:'
 p tree.postorder
-puts "Elements in order:"
+puts 'Elements in order:'
 p tree.inorder
-puts "Adding elements to unbalance tree"
+puts 'Adding elements to unbalance tree'
 tree.insert(123)
 tree.insert(32423)
 tree.insert(102)
 tree.insert(400)
 tree.pretty_print
 puts "Is the tree balanced: #{tree.balanced?}"
-puts "Rebalancing tree."
+puts 'Rebalancing tree.'
 tree.rebalance
 tree.pretty_print
 puts "Is the tree balanced: #{tree.balanced?}"
-puts "Elements in level order:"
+puts 'Elements in level order:'
 p tree.level_order
-puts "Elements in preorder:"
+puts 'Elements in preorder:'
 p tree.preorder
-puts "Elements in postorder:"
+puts 'Elements in postorder:'
 p tree.postorder
-puts "Elements in order:"
+puts 'Elements in order:'
 p tree.inorder
